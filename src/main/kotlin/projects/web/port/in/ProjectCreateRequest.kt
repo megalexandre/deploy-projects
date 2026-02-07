ae331e36ada4f7c234f46d7f41c03949b01c7704
Alexandre <megalexandre@gmail.com>
2026-02-07 11:07:11 -0300

    start Project.kt

--- src/main/kotlin/projects/web/port/in/ProjectCreateRequest.kt
+++ src/main/kotlin/projects/web/port/in/ProjectCreateRequest.kt
@@ -5,11 +5,11 @@ import jakarta.validation.constraints.Size
 import projects.core.model.Project
 import java.util.*
 
-
 class ProjectCreateRequest (
 
     @field:NotBlank(message = "{project.name.notBlank}")
     @field:Size(min = 2, message = "{project.name.size}")
+    @field:Size(max = 250, message = "{project.name.size}")
     val name: String
 ){
     fun toDomain() = Project(
